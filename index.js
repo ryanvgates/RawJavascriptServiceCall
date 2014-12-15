0a7e7ede7b1d73b610253c712e0ddb45ef2fd79e
Ryan Gates <ryanvgates@gmail.com>
2014-12-15 13:29:22 -0500

    Got the request displaying below when the button is clicked

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function sendRequest(){
-	
+	var response = document.getElementById('response');
+    response.className = 'show';
+    var responseAddress = document.getElementById('responseAddress');
+    responseAddress.innerHTML = serviceAddress.value;
 };
 
-document.addEventListener("DOMContentLoaded", function(event) {
-	document.getElementByTagName('btnSend').
+document.addEventListener('DOMContentLoaded', function(event) {
+	document.getElementById('btnSend').addEventListener('click', sendRequest, false);
 });
